e20c8c31ae97e40e03068d6ebaf33b250b478a4b
Carlos Guzmán Ramírez <cguzmanr17@gmail.com>
2025-12-01 23:19:19 -0600

    Fixed deployment issue.

--- portfolio/src/components/common/ProjectCard.tsx
+++ portfolio/src/components/common/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { type Project } from "../../types/IProject";
 import { slugify } from "../../utils/helpers";
